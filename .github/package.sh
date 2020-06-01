c4a50ade71487527fb754573dd8b4bb82d7306db
Olaf Reitmaier <olafrv@gmail.com>
2020-06-01 21:24:33 +0200

    Improving Package Creation

--- .github/package.sh
+++ .github/package.sh
@@ -1,10 +1,14 @@
 # https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line
 
 # Step 0: Build Local Image
-docker build -t md2cf .
+docker image rm md2cf:latest
+docker image rm docker.pkg.github.com/olafrv/md2cf/md2cf:1.0.0-rc1
+docker build -t md2cf ..
 # Step 1: Authenticate
 echo $GH_TOKEN | docker login docker.pkg.github.com -u olafrv --password-stdin
 # Step 2: Tag
 docker tag md2cf:latest docker.pkg.github.com/olafrv/md2cf/md2cf:1.0.0-rc1
+# Step 4: Inspect
+docker run --rm -it --entrypoint /bin/bash docker.pkg.github.com/olafrv/md2cf/md2cf:1.0.0-rc1
 # Step 3: Publish
 docker push docker.pkg.github.com/olafrv/md2cf/md2cf:1.0.0-rc1
